381d696cb4fd7db78801550d8ac34a4f4c1ce805
Prajwal Poudel <prajwalpoudel2002@gmail.com>
2024-09-26 07:18:59 +0545

    login-design

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -24,6 +24,7 @@ class ViewServiceProvider extends ServiceProvider
     {
         View::composer(['admin.*'], MenuComposer::class);
         View::composer(['admin.*'], SettingsComposer::class);
+        View::composer(['auth.*'], SettingsComposer::class);
 
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,57 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('auth.layouts')
+@section('title')
+   MBCOE LOGIN
+@endsection
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+@section('content')
+    <div class="kt-login__container">
+        <div class="kt-login__logo">
+            <a href="#">
+                <img src="{{ getImageUrl($setting->logo) }}" height="100" width="100">
+            </a>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        <div class="kt-login__signin">
+            <div class="kt-login__head">
+                <h3 class="kt-login__title">Sign In To Admin</h3>
+            </div>
+            <form class="kt-form" method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="input-group">
+                    <input class="form-control" type="text" placeholder="Email" name="email" autocomplete="off">
+                </div>
+                <div class="input-group">
+                    <input class="form-control" type="password" placeholder="Password" name="password">
+                </div>
+                <div class="row kt-login__extra">
+                    <div class="col">
+                        <label class="kt-checkbox">
+                            <input type="checkbox" name="remember"> Remember me
+                            <span></span>
+                        </label>
+                    </div>
+                    <div class="col kt-align-right">
+                        <a href="javascript:;" id="kt_login_forgot" class="kt-login__link">Forget Password ?</a>
+                    </div>
+                </div>
+                <div class="kt-login__actions">
+                    <button type="submit" class="btn btn-brand btn-pill kt-login__btn-primary">Sign In</button>
+                </div>
+            </form>
         </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
+        <div class="kt-login__forgot">
+            <div class="kt-login__head">
+                <h3 class="kt-login__title">Forgotten Password ?</h3>
+                <div class="kt-login__desc">Enter your email to reset your password:</div>
+            </div>
+            <form class="kt-form" action="">
+                <div class="input-group">
+                    <input class="form-control" type="text" placeholder="Email" name="email" id="kt_email" autocomplete="off">
+                </div>
+                <div class="kt-login__actions">
+                    <button id="kt_login_forgot_submit" class="btn btn-brand btn-pill kt-login__btn-primary">Request</button>&nbsp;&nbsp;
+                    <button id="kt_login_forgot_cancel" class="btn btn-secondary btn-pill kt-login__btn-secondary">Cancel</button>
+                </div>
+            </form>
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+@endsection
