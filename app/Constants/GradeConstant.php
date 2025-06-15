91b56386c7c39b60ad0aecc09f81b2e3ab3473a3
Prajwal Poudel <prajwalpoudel2002@gmail.com>
2025-06-15 12:50:12 +0545

    Grade Management

--- app/Constants/GradeConstant.php
+++ app/Constants/GradeConstant.php
@@ -7,11 +7,35 @@ namespace App\Constants;
 class GradeConstant
 {
     const A = 'A';
+    const AMINUS = 'A-';
+    const BPLUS = 'B+';
     const B = 'B';
+    const BMINUS = 'B-';
+    const CPLUS = 'C+';
     const C = 'C';
+    const CMINUS = 'C-';
+    const DPLUS = 'D+';
     const D = 'D';
+    const DMINUS = 'D-';
     const ABS = 'Abs';
+    const FAIL = 'F';
+    const CNR = 'Cnr';
     const EXPELLED = 'Expelled';
 
-    const GRADES = [self::A => self::A, self::B => self::B, self::C => self::C,  self::D => self::D, self::ABS => self::ABS, self::EXPELLED => self::EXPELLED];
+    const GRADES = [
+        self::A => self::A,
+        self::AMINUS => self::AMINUS,
+        self::BPLUS => self::BPLUS,
+        self::B => self::B,
+        self::BMINUS => self::BMINUS,
+        self::CPLUS => self::CPLUS,
+        self::C => self::C,
+        self::CMINUS => self::CMINUS,
+        self::DPLUS => self::DPLUS,
+        self::D => self::D,
+        self::ABS => self::ABS,
+        self::FAIL => self::FAIL,
+        self::CNR => self::CNR,
+        self::EXPELLED => self::EXPELLED,
+    ];
 }
